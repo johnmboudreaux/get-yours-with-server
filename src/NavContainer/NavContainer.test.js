484afe9c4ce6c627d12fe086bdd9c14e675531cd
johnmboudreaux <jhnbdrx@gmail.com>
2017-11-08 09:47:41 -0700

    trying to test navContainer state and refactored a little in navContainer

--- src/NavContainer/NavContainer.test.js
+++ src/NavContainer/NavContainer.test.js
@@ -1,12 +1,32 @@
-import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
+import { shallow, mount } from 'enzyme';
 import NavContainer from './NavContainer';
 import React from 'react';
+import { MemoryRouter } from 'react-router';
+
+const middleware = [];
+const mockStore = configureStore(middleware);
+const initialState = false;
+const store = mockStore(initialState);
 
 describe('NavContainer snapshot', () => {
 
   it('should always match the snapshot', () => {
-    const wrapper = shallow(<NavContainer />);
+    const wrapper = shallow(<NavContainer store={store} />);
 
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('toggleMenu should update state', () => {
+    const search = mount(<NavContainer />);
+
+    const button = search.find('button');
+
+    expect(search.state().menuShowing).toEqual(false);
+
+    button.simulate('click');
+
+    expect(search.state().menuShowing).toEqual(true);
+  });
+
 });
