4f9dcf0bb394d08ebf18bf74878b2281b32b4c41
johnmboudreaux <jhnbdrx@gmail.com>
2018-02-28 12:35:31 -0700

    changed path in actions

--- src/CampingCatalog/Actions.js
+++ src/CampingCatalog/Actions.js
@@ -1,4 +1,4 @@
-import cleanAmazonData from '../Utils/cleanAmazonData';
+import cleanAmazonData from './src/Utils/cleanAmazonData';
 
 export const loadCampingProductSuccess = (campingProducts) => {
   return {
--- src/RecommendedCatalog/Actions.js
+++ src/RecommendedCatalog/Actions.js
@@ -1,4 +1,4 @@
-import cleanAmazonData from '../Utils/cleanAmazonData';
+import cleanAmazonData from './src/Utils/cleanAmazonData';
 
 export const loadRecommendedProductSuccess = (recommendedProducts) => ({
   type: 'LOAD_RECOMMENDED_PRODUCTS',
